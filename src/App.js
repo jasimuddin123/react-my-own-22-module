4a5fc6da0f82596207b5ead6bf82dcf7dfa9fb3c
jasimuddin123 <jasimuddindxn@gmail.com>
2020-06-01 08:30:31 -0700

    22-4 JSX, Dynamic content, Dynamic Style in React

--- src/App.js
+++ src/App.js
@@ -2,12 +2,29 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  
+  var name ='Jasim Uddin';
+  const person ={
+    name:'Jasim Uddin',
+    age:33,
+    job:'Supervisor'
+  }
+
+  const player ={
+    name:'Ibrahim',
+    country:'Bangladesh',
+    age: 33,
+  }
+  const style ={
+    color:'red',
+    fontWeight:'bold',
+    backgroundColor:'green',
+  }
   return (
     <div>
      <h4>This is react Person </h4>
-     <h2>I am Lazy react person</h2>
-     <h3>This part only explain about react </h3>
+  <h2 style={{color:'cyan'}}> Person Details:{person.name + " " + person.job}</h2>
+     <h3>Name :{name} </h3>
+  <h3 className="" style={style}>Player Details : {player.name + " " + player.age}</h3>
    </div>
   );
 }
