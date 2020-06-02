7e175e9d6cd82fdb4a58684919d30c0278ee0eea
jasimuddin123 <jasimuddindxn@gmail.com>
2020-06-02 06:31:54 -0700

    22-6 Create Component, return multiple HTML from a component

--- src/App.js
+++ src/App.js
@@ -19,18 +19,40 @@ function App() {
  const personStyle ={
    color:'cyan',
    backgroundColor:'green',
-   border:'1px solid lightgray'
+   border:'1px solid lightgray',
+   width: '300px'
  }
   
   return (
-    <h1>This Number 22-5 Part onl explain about Component identity </h1>
+    
     <div style={personStyle}className="information">
+     
       <h3 style={{color:'red'}}>Name:{name}</h3>
       <h2>Person Details: {person.name + " " + person.job }</h2>
       <h3>Player Details: {player.name + " " + player.country}</h3>
  
+   <Basic></Basic>
+   
+   <Basic2></Basic2>
+   <Basic2></Basic2>
+
    </div>
+  
   );
+}
+
+// Basic Component 
+function Basic(){
+  return <h2 style={{margin:'20px'}}>This is Basic Component</h2>
+}
+
+// Another Basic 
+function Basic2(){
+  return(
+    <div style={{border:'1px solid gray', margin:'20px'}}>
+      <h2>This is Basic two component</h2>
+    </div>
+  )
 }
 
 
