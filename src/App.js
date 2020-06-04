ad344a7b619f3541bd515a955ae6eb8b0e14547c
jasimuddin123 <jasimuddindxn@gmail.com>
2020-06-04 07:07:19 -0700

    22-9 Create multiple components from an array of objects

--- src/App.js
+++ src/App.js
@@ -1,128 +1,146 @@
 import React from 'react';
 import './App.css';
 
+
+
+const schollerName = ['Mizanur Rahman', 'Mufti Kazi Ibrahim', 'Mulla Nazim'];
+
+const heroName = ['Jasim', 'Kamal', 'Milon', 'Sharef'];
+
+
+
+const person = {
+  name: 'Zahid Iqbal',
+  age: '50',
+  job: 'Forklift Operator'
+}
+
+const player = {
+  name: 'Munna',
+  age: '55',
+  profession: 'Soccer Player'
+}
+
 function App() {
 
   const products = [
-    { name: 'Photoshop', price: '$200.44' },
-    { name: 'Illustrator', price: '$333.33' },
-    { name: 'PDF Reader', price: '$20.20' },
-    { name: 'Indesign', price: '$100.33' }
+
+    { name: 'Photoshop', price: '$200.55' },
+    { name: 'Illustrator', price: '$100.33' },
+    { name: 'Adove Reader', price: '$250.20' },
+    { name: 'Premium', price: '$100.20' }
 
   ]
-  const info = {
-    name: 'Sonabondu',
-    profession: 'Serviceholder',
-    salary: '3400 Sr'
-
-  }
-  const name = 'Jasim uddin';
-  const schollerName = ['Zakir Nayek', 'Mizanur Rahman', 'Mufti Ibrahim'];
-  const person = {
-    name: 'Jasim Uddin',
-    job: 'Supervisor',
-    age: '33'
-  }
-
-  const player = {
-    name: 'Ibarahim',
-    country: 'Bangladesh',
-    age: '45'
-  }
 
   const personStyle = {
-    color: 'cyan',
-    backgroundColor: 'green',
-    border: '1px solid lightgray',
-    width: '300px'
+    color: 'green',
+    backgroundColor: 'gray',
+    width: '300px',
+
+    border: '5px solid green',
+
+
   }
 
+  const playerStyle = {
+    color: 'blue',
+    backgroundColor: 'yellow',
+    fontWeight: 'bold',
+    borderRadius: '2px',
+    border: '10px solid gold',
+    margin: '10px'
+  }
   return (
 
-    <div style={personStyle} className="information">
 
-      <h3 style={{ color: 'red' }}>Name:{name}</h3>
-      <h2>Person Details: {person.name + " " + person.job}</h2>
-      <h3>Player Details: {player.name + " " + player.country}</h3>
 
-      {/* <Product name={products[0].name} price={products[0].price}></Product> */}
+    <div style={personStyle}>
+      <h3> Name :{person.name + " " + person.age}</h3>
+      <h4>Age:{person.age}</h4>
+      <h4>Profession:{person.job}</h4>
+      <ul style={{ color: 'white' }}>
+        {
+          heroName.map(hero => <li>{hero}</li>)
+        }
+        {
+          products.map(pd => <li>{pd.name}</li>)
+        }
+        {
+          products.map(product => <Product product={product}></Product>)
+        }
 
-      <Product product={products[0]}></Product>
-      <Product product={products[1]}></Product>
-      <Product product={products[2]}></Product>
+      </ul>
 
+      {
+        products.map(pd => <Product product={pd}></Product>)
+      }
+
+      <h2 style={playerStyle}>Player Name: {player.name}</h2>
+
+      <h3 style={{ color: 'gold', fontSize: '40px', border: '10px solid' }}>Player profeesion:{player.profession}</h3>
       <Basic></Basic>
-      <Basic> </Basic>
-      <Basic2 scholler={schollerName[1]} food="Protin"></Basic2>
-      <Basic2 scholler="Mizanur Rahman" food="Desi Food"></Basic2>
-      <Basic2 singer="Zain"></Basic2>
 
-      <Information name={info.name} profession={info.profession} salary={info.salary}></Information>
+      <Basic2 scholler={schollerName[0]} food="Vegitable"></Basic2>
+      <Basic2 name="Jasim Uddin" profession="Supervisior" experience="15 years"></Basic2>
 
+      <Product product={products[0]}> </Product>
+      <Product></Product>
+      <Product></Product>
     </div>
+
 
   );
 }
 
-// Basic Component 
-function Basic(props) {
+function Basic() {
 
-  return <h2 style={{ margin: '20px' }}>This is Basic Component</h2>
+  const basicStyle = {
+    color: 'yellow',
+    backgroundColor: 'gray'
+  }
+  return <h2 style={basicStyle}>This is Basic Component All the time</h2>
 }
 
-// Another Basic 
 function Basic2(props) {
 
-  return (
-
-    <div style={{ border: '1px solid gray', margin: '20px', width: '500px', backgroundColor: 'gray' }}>
-      <h2>Scholler Details :{props.scholler}</h2>
-      <h3>Scholler Food: {props.food}</h3>
-      <h3>Singer Name:{props.singer}</h3>
-
-    </div>
-  )
-}
-
-function Information(props) {
-  const informationStyle = {
-    color: 'green',
-    border: '1px solid gold',
-    backgroundColor: 'lightGray',
-    width: '500px',
-    height: '500px'
+  const basic2Style = {
+    color: 'cyan',
+    background: 'gray'
   }
   return (
-    <div style={informationStyle} className="information">
-      <h3>Name{props.name}</h3>
-      <h3>Profession: {props.profession}</h3>
-      <h3>Salary:{props.salary}</h3>
+    <div style={basic2Style} className="">
+      <h3>Scholler Name :{props.scholler + " " + props.food}</h3>
+      <h4>Profession: {props.profession}</h4>
+      <h4>Experience:{props.experience}</h4>
     </div>
+
   )
+
 }
 
 function Product(props) {
 
   const productStyle = {
-    color: 'green',
+    color: 'cyan',
+    backgroundColor: 'gray',
+    float: 'right',
     width: '300px',
     height: '200px',
-    backgroundColor: 'lightgray',
-    borderRadius: '5px',
-    border: '2px solid green',
-    float: 'left',
+    border: '5px solid green',
+    textAlign: 'center'
   }
-  const { name, price } = props.product;
+
 
   return (
-    <div style={productStyle} className="product">
-      <h2>Name:{name}</h2>
-      <h2>Price:{price}</h2>
+    <div style={productStyle}>
+
+      <h2> Product:</h2>
+      <h3>Price:</h3>
       <button>Buy Now</button>
+
     </div>
+
   )
-
 }
-
 
 export default App;
